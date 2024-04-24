e25c13e6cec675ce91bbdbdcbbbac3cb48bbf869
Jhonatan <jhonatan.dev90@gmail.com>
2024-04-24 19:24:23 -0300

    Adicionando informações sobre os projetos do portfólio.

--- script.js
+++ script.js
@@ -3,40 +3,53 @@ const listaPortfolio = document.querySelector(".listaPortfolio");
 
 const dadosPortfolio = [
   {
-    name: "Nome da aplicação",
-    imagem: "Imagem da aplicação",
-    descricao: "Breve descrição da aplicação",
-    tecnologias: ["Lista de tecnologias", "HTML", "CSS", "JavaScript"],
+    name: "Projeto Sorteador",
+    imagem: "./images/readmeSorteador.png",
+    descricao:
+      "Sorteador de nomes, criado através de uma aplicação interativa e funcional, de maneira simples e eficiente. Os usuários podem inserir uma lista de nomes e obter um nome sorteado aleatoriamente.",
+    tecnologias: ["HTML", "CSS", "JavaScript"],
+    linkProjeto: "https://jhonatansilva90.github.io/Projeto-Sorteador/",
   },
   {
-    name: "Nome da aplicação",
-    imagem: "Imagem da aplicação",
-    descricao: "Breve descrição da aplicação",
-    tecnologias: ["Lista de tecnologias", "HTML", "CSS", "JavaScript"],
+    name: "Verificador de Previsão do Tempo",
+    imagem: "./images/readmeVerificadorTemperatura.png",
+    descricao:
+      "Projeto de verificação de temperatura, com exibição de informações de temperatura em °C, nuvens e umidade das cidades pesquisadas.",
+    tecnologias: ["HTML", "CSS", "JavaScript"],
+    linkProjeto:
+      "https://jhonatansilva90.github.io/Verificador-de-Previs-o-do-Tempo/",
   },
   {
-    name: "Nome da aplicação",
-    imagem: "Imagem da aplicação",
-    descricao: "Breve descrição da aplicação",
-    tecnologias: ["Lista de tecnologias", "HTML", "CSS", "JavaScript"],
+    name: "To Do List",
+    imagem: "./images/readmeToDoList.png",
+    descricao:
+      "O Projeto To Do List permite ao usuário a criação de uma lista de tarefas para auxilia-lo no dia a dia. Ele oferece a possibilidade de criação e exclusão de tarrefas, definir tarefas como realizadas e limpar toda a lista de tarefa criada.",
+    tecnologias: ["HTML", "CSS", "JavaScript"],
+    linkProjeto: "https://jhonatansilva90.github.io/To-Do-List/",
   },
   {
-    name: "Nome da aplicação",
-    imagem: "Imagem da aplicação",
-    descricao: "Breve descrição da aplicação",
-    tecnologias: ["Lista de tecnologias", "HTML", "CSS", "JavaScript"],
+    name: "Página de Criptomoedas",
+    imagem: "./images/readmePaginaCriptomoedas.png",
+    descricao:
+      "Página web com informações sobre criptomoedas. A página exibi as informações de maneira clara, além disso, o sistema permiti o usuário fazer conversões entre os preços das moedas.",
+    tecnologias: ["HTML", "CSS", "JavaScript"],
+    linkProjeto: "https://jhonatansilva90.github.io/Pagina-Criptomoeda/",
   },
   {
-    name: "Nome da aplicação",
-    imagem: "Imagem da aplicação",
-    descricao: "Breve descrição da aplicação",
-    tecnologias: ["Lista de tecnologias", "HTML", "CSS", "JavaScript"],
+    name: "Cronômetro",
+    imagem: "./images/readmeCronometro.png",
+    descricao:
+      "O projeto é um cronômetro, criado através de uma aplicação interativa e funcional. Por meio de uma interface simples, os usuários podem iniciar, pausar, reiniciar e resetar o cronômetro.",
+    tecnologias: ["HTML", "CSS", "JavaScript"],
+    linkProjeto: "https://jhonatansilva90.github.io/Cronometro/",
   },
   {
-    name: "Nome da aplicação",
-    imagem: "Imagem da aplicação",
-    descricao: "Breve descrição da aplicação",
-    tecnologias: ["Lista de tecnologias", "HTML", "CSS", "JavaScript"],
+    name: "Buscador de Endereços",
+    imagem: "./images/readmeBuscadorEnderecos.png",
+    descricao:
+      "Integrando-se à API do ViaCEP, a aplicação oferece uma interface simples, permitindo que os usuários digitem o CEP desejado e obtenham em retorno informações detalhadas, como Rua, Bairro, Cidade e Estado.",
+    tecnologias: ["HTML", "CSS", "JavaScript"],
+    linkProjeto: "https://jhonatansilva90.github.io/buscarEndereco-ViaCEP/",
   },
 ];
 
@@ -45,9 +58,14 @@ dadosPortfolio.forEach((portfolio) => {
   card.classList.add("card");
   card.innerHTML = `
   <h4>${portfolio.name}<h4>
-  <img src="${portfolio.imagem}" alt="${portfolio.name}">
+  <img class="imgProjetos" src="${portfolio.imagem}" alt="${portfolio.name}">
   <p>${portfolio.descricao}</p>
   <p>${portfolio.tecnologias.join(" | ")}</p>
   `;
+  // Adicionando um event listener ao card
+  card.addEventListener("click", () => {
+    window.open(portfolio.linkProjeto, "_blank");
+  });
+
   listaPortfolio.appendChild(card);
 });
